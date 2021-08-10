7abf839f834b448a668b8269ebdb8a12c5e74647
arjshriv <arjitshrivastava@yahoo.in>
2021-08-10 20:40:13 +0530

    First Commit

---
+++ user_contact/src/test/java/com/contact/user_contact/UserContactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contact.user_contact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserContactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
